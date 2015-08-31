9784cdd9a125e37889ec624a1c422829ed131520
Mark Turiansky <mark.turiansky@yahoo.com>
2015-08-30 20:01:06 -0400

    major ui updates

--- custom.js
+++ custom.js
@@ -7,3 +7,18 @@ $("a[href^='#']").on('click', function(e) {
        window.location.hash = hash;
    });
 });
+
+$(document).scroll(function() {
+    var t = $(this).scrollTop() + $(window).height() / 2;
+
+    $('.active').removeClass('active');
+    if(t < $('#about').position().top) {
+        $('#main-button').addClass('active');
+    } else if(t >= $('#about').position().top && t < $('#resume').position().top) {
+        $('#about-button').addClass('active');
+    } else if(t >= $('#resume').position().top && t < $('#contact').position().top) {
+        $('#resume-button').addClass('active');
+    } else if(t >= $('#contact').position().top) {
+        $('#contact-button').addClass('active');
+    }
+});
