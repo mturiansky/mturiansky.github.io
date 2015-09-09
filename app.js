944e27bbfc9d54b3cb789fe76e7443a983905dd6
Mark Turiansky <mark.turiansky@yahoo.com>
2015-09-08 23:59:49 -0400

    minor updates

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 var express = require('express');
+var swig = require('swig');
 var app = express();
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 app.use(express.static('static'));
 
-var server = app.listen(3000, function() {
+app.use(function (req, res, next) {
+    var d = new Date();
+    console.log('[%s][/%s] Request from %s', d.toLocaleTimeString(), req.baseUrl, req.ip);
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.render('main', {}, function (err, html) {
+        res.send(html);
+    });
+});
+
+var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Listening on http://%s:%s', host, port);
+    var d = new Date();
+    console.log('[%s] Listening on http://%s:%s', d.toLocaleTimeString(), host, port);
 });
